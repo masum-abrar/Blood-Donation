157ed7833f1fb20ff977e0970ca324ffcaf9c5e3
masum-abrar <masum.abrar02@gmail.com>
2024-06-22 21:26:59 +0600

    payment system updated

--- src/pages/AllBlogs.jsx
+++ src/pages/AllBlogs.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import Swal from 'sweetalert2';
 import { useAxiosSecure } from '../hooks/useAxiosSecure';
+import { Helmet } from 'react-helmet-async';
 
 export const AllBlogs = () => {
   const axiosSecure = useAxiosSecure();
@@ -30,6 +31,10 @@ export const AllBlogs = () => {
 
   return (
     <div className="container mx-auto p-4 relative top-12">
+         <Helmet>
+                <title>Blood | Blog</title>
+            </Helmet>
+            
       <h2 className="text-3xl font-bold mb-4 text-center">All Blogs</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
